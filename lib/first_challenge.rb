56e06eba0d492c3cd934e931241341f5e8f885cc
Berkley Brown <berkleydeane@gmail.com>
2020-06-02 00:09:29 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -3,17 +3,6 @@ def first_challenge
 
 
 epic_tragedy = {
-   :montague => {
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => []
-   },
-  :capulet => {
-    :patriarch => {},
-      :matriarch => {},
-      :heroine => {},
-      :heroine_friends => []
-  }
+   
 }
 end
